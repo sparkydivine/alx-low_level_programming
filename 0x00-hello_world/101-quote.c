467c84685f4adc33b730af101f31c957a8c0d5af
sparkydivine <olisaebube0@gmail.com>
2023-02-18 01:40:00 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry
+ * main - A program that print without pritg and put function
+ * Description: Prints "and that piece of art is useful.." without puts
  * Return: 1
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
-	return (1);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
 }
