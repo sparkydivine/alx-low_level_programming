b4e83e18bb8397ce564b7624988602c0339bd2b2
sparkydivine <olisaebube0@gmail.com>
2023-03-10 12:09:55 -0800

    static

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @a: checked parameter
+ *
+ * Return: always a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
